be9a6d31aca5d73228498e65af00b6030abaa47e
CptDoritos <fred.vezina@hotmail.com>
2024-02-13 00:10:47 -0500

    Revert "Revert "nextjsconversion""
    This reverts commit 3b3c7fd5d7762a73e93cbc63be398b9c4b9974d1.

--- src/app/page.js
+++ src/app/page.js
@@ -1,50 +1,27 @@
-'use client'
-import Projects from "./pages/projects.js"
-import Contact from "./pages/contact.js"
-import Education from "./pages/education.js"
-import HomePage from "./pages/homepage.js"
-import FallenGods from "./pages/fallengods.js"
-import QuickReload from "./pages/quickreload.js"
-import PetStore from "./pages/petstore.js"
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { createContext, useEffect } from 'react'
+import HomePage from "../components/pages/homepage.js"
+import Projects from "../components/pages/projects.js"
+import Education from "../components/pages/education.js"
+import Contact from "../components/pages/contact.js"
+import FallenGods from "../components/pages/fallengods.js"
+import QuickReload from "../components/pages/quickreload.js"
+import PetStore from "../components/pages/petstore.js"
+import Link from 'next/link';
 
-const Context = createContext();
 export default function Home() {
-
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <HomePage/>
-    },
-  
-    {
-      path: "/projects",
-      element: <Projects/>
-    },
-    {
-      path: "/education",
-      element: <Education/>
-    },
-    {
-      path: "/contact",
-      element: <Contact/>
-    },
-    {
-      path: "/fallenGods",
-      element: <FallenGods/>
-    },
-    {
-      path: "/quickReload",
-      element: <QuickReload/>
-    },
-    {
-      path: "/petstore",
-      element: <PetStore/>
-    }
-  ]);
-
-    return (
-      <RouterProvider router={router} />
+  return (
+    <div>
+      <nav>
+        <Link href="/"><a>Home</a></Link>{' '}
+        <Link href="/projects"><a>Projects</a></Link>{' '}
+        <Link href="/education"><a>Education</a></Link>{' '}
+        <Link href="/contact"><a>Contact</a></Link>{' '}
+        <Link href="/fallenGods"><a>Fallen Gods</a></Link>{' '}
+        <Link href="/quickReload"><a>Quick Reload</a></Link>{' '}
+        <Link href="/petstore"><a>Pet Store</a></Link>
+      </nav>
+      <main>
+        <h1>Welcome to Next.js!</h1>
+      </main>
+    </div>
   );
 }
